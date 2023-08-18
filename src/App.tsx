656479a6d2930f1ea8c1f36dfc1027fcc1c13261
ocampco <ocampco@gmail.com>
2023-08-18 17:07:36 +0700

    Set default lang to english

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,17 @@
-const App = () => <h1>small talk translator</h1>
+import { Navigate, useParams } from 'react-router-dom';
+import { LOCALE_NATIVE_DEFAULT } from './constants/config';
+
+const App = () => {
+  const { nativeLocale } = useParams();
+
+  return (
+    <>
+      { !nativeLocale &&
+          <Navigate to={LOCALE_NATIVE_DEFAULT} replace={true} />
+      }
+      <h1>small talk translator</h1>
+    </>
+  );
+};
 
 export default App;
--- src/constants/config.js
+++ src/constants/config.js
@@ -1,1 +1,2 @@
 export const ROUTES_BASENAME = '/small-talk';
+export const LOCALE_NATIVE_DEFAULT = 'en';
