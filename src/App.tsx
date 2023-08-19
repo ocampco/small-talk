1d10e2d070cdfb87d6140caab05137a7b3675054
ocampco <ocampco@gmail.com>
2023-08-19 12:34:13 +0700

    Add functionality for translation language

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
 import { Navigate, useParams } from 'react-router-dom';
-import locales, { Locale, LOCALE_DEFAULT } from './constants/locales';
+import locales, {
+  Locale,
+  LOCALE_DEFAULT,
+  LOCALE_NOT_SELECTED,
+} from './constants/locales';
 
 const App = () => {
-  const { nativePath } = useParams();
+  const { nativePath, translationPath } = useParams();
   // TODO: Make distinction between not found and not given
-  const nativeLocale: Locale = locales.get(nativePath) || LOCALE_DEFAULT;
+  const nativeLocale: Locale = locales.get(nativePath)
+    || LOCALE_DEFAULT;
+  const translationLocale: Locale = locales.get(translationPath)
+    || LOCALE_NOT_SELECTED;
 
   return (
     <>
@@ -13,6 +20,7 @@ const App = () => {
       }
       <h1>small talk translator</h1>
       <label>translate from:</label><span>{nativeLocale.display}</span>
+      <label>translate to:</label><span>{translationLocale.display}</span>
     </>
   );
 };
--- src/constants/locales.ts
+++ src/constants/locales.ts
@@ -5,14 +5,20 @@ export type Locale = {
 
 const LOCALE_ENGLISH: Locale = {
   code: 'en',
-  display: 'English',
+  display: 'english',
 };
 
 const LOCALE_THAI: Locale = {
   code: 'th',
-  display: 'Thai',
+  display: 'thai',
 };
 
+export const LOCALE_DEFAULT: Locale = LOCALE_ENGLISH;
+// TODO: URL Logic for not selected
+export const LOCALE_NOT_SELECTED: Locale = {
+  code: '',
+  display: 'please select',
+}
 
 const localesArray: Locale[] = [LOCALE_ENGLISH, LOCALE_THAI];
 const localesMap: Map<string | undefined, Locale> = new Map();
@@ -21,5 +27,4 @@ localesArray.map(
   (locale: Locale) => localesMap.set(locale.code, locale)
 );
 
-export const LOCALE_DEFAULT: Locale = LOCALE_ENGLISH;
 export default localesMap;
